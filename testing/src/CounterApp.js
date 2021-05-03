dcd73afb077bb93741613d5e0fc743e0c568d65e
Arumuga priya <arumugapriya@smitiv.co>
2021-05-03 13:53:34 +0530

    snapshot testing updated

--- testing/src/CounterApp.js
+++ testing/src/CounterApp.js
@@ -13,14 +13,23 @@ class CounterApp extends Component {
       count: prevState.count + amount
     }));
 
+  makeDecrementor = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count - amount
+    }));
+
   increment = this.makeIncrementer(1);
+  decrement = this.makeDecrementor(1);
 
   render() {
     return (
       <div>
         <p>Count: {this.state.count}</p>
-        <button className="increment" onClick={this.increment}>
+        <button className="Increment" onClick={this.increment}>
           Increment count
+        </button>
+        <button className="Decrement" onClick={this.decrement}>
+          Decrement count
         </button>
       </div>
     );
--- testing/src/CounterApp.test.js
+++ testing/src/CounterApp.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import CounterApp from "./CounterApp";
+import renderer from "react-test-renderer";
 
 describe("Counter component", () => {
   it("starts with a count of 0", () => {
@@ -18,12 +19,38 @@ describe("test button click", () => {
     //   count++;
     // };
     const button = shallow(
-      <button className="increment" onClick={mockCallBack}>
+      <button className="Increment" onClick={mockCallBack}>
         Increment count
       </button>
     );
     button.find("button").simulate("click");
     expect(mockCallBack.mock.calls.length).toEqual(1);
     // expect(count).toEqual(0);
+  });
+});
+
+describe("decrement the count", () => {
+  it("decrement value", () => {
+    const wrapper = shallow(<CounterApp />);
+    const decbtn = wrapper.find("button.Decrement");
+    decbtn.simulate("click");
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: -1");
+
+    const mockCallBack = jest.fn();
+    const decBtnClick = shallow(
+      <button className="Decrement" onClick={mockCallBack}>
+        Decrement count
+      </button>
+    );
+    decBtnClick.find("button").simulate("click");
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+});
+
+describe("test snap", () => {
+  it("matches the snapshot", () => {
+    const tree = renderer.create(<CounterApp />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
